c2583f48a0bfa405c35ff365d55d444835fe6abb
gamzeakyol06 <gamzeakyol06@gmail.com>
2023-02-16 16:26:03 +0300

    Initial commit

---
+++ src/main/java/Base/Base.java
@@ -0,0 +1,33 @@
+package Base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+    protected static WebDriver driver;
+
+    public final static String MAIN_PAGE_URL = "https://www.google.com/";
+
+    public void beforemethod(){
+        System.setProperty("webdriver.chrome.driver","src/main/driver/chromedriver.exe");
+        System.out.println(System.getProperty("webdriver.chrome.driver"));
+ /*       chromeOptions.addArguments("--whitelist-ip *");
+        chromeOptions.addArguments("--proxy-server='direct://'");
+        chromeOptions.addArguments("--proxy-bypass-list=*");
+        chromeOptions.addArguments("--ignore-certificate-errors");
+        chromeOptions.addArguments("test-type");
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--disable-web-security");
+        chromeOptions.addArguments("--allow-running-insecure-content");
+        chromeOptions.setAcceptInsecureCerts(true);
+        chromeOptions.setHeadless(true);
+        chromeOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+        chromeOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+        //test.eldorturkey.com.tr
+        // test.eldorturkey.com.tr*/
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    public void aftermethod(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/Test/MainPage_Test.java
@@ -0,0 +1,37 @@
+package Test;
+import Base.Base;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
+public class MainPage_Test extends Base {
+    @BeforeTest
+    public void BeforeMethod() {
+        super.beforemethod();
+    }
+    @Test
+    public void Page_Test() throws InterruptedException {
+        driver.get(MAIN_PAGE_URL);
+        Response response = given().
+                when()
+                .get(MAIN_PAGE_URL)
+                .then()
+                .extract().response();
+        System.out.println("Application Status is "  + response.getStatusCode());
+        if (response.getStatusCode() == 200) {
+            System.out.println("Test is OK");
+        } else {
+            System.out.println("Test is NOK");
+            Assert.fail();
+        }
+        Thread.sleep(1000);
+    }
+
+    @AfterTest
+    public void AfterMethod() {
+        super.aftermethod();
+    }
+}
