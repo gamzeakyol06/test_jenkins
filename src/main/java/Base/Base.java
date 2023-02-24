f8966312f92afcef21ee2f70b53901e000424b0b
gamzeakyol06 <gamzeakyol06@gmail.com>
2023-02-24 16:51:58 +0300

    @FindBy(css="") 	private WebElement webElement; first commit

--- src/main/java/Base/Base.java
+++ src/main/java/Base/Base.java
@@ -15,7 +15,7 @@ public class Base {
     public final static String MAIN_PAGE_URL = "https://www.google.com/";
 
     public void beforemethod() throws MalformedURLException {
-        System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/workspace/test_github/src/main/driver/chromedriver");
+        System.setProperty("webdriver.chrome.driver","src/main/driver/chromedriver");
         System.out.println(System.getProperty("webdriver.chrome.driver"));
 
 
