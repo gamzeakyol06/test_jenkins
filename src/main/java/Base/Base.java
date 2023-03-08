9df58b1e227922f63fd70fab77371dc54cde431a
gamzeakyol06 <gamzeakyol06@gmail.com>
2023-03-08 16:39:35 +0300

    Merge remote-tracking branch 'github/main'
    # Conflicts:
    #	src/main/java/Base/Base.java

--- src/main/java/Base/Base.java
+++ src/main/java/Base/Base.java
@@ -28,7 +28,7 @@ public class Base {
         //chromeOptions.addArguments("--headless");
         chromeOptions.addArguments("--no-sandbox");
 
-        driver = new RemoteWebDriver(new URL("http://46.101.220.229:4444/ui"), chromeOptions);
+        WebDriver driver = new RemoteWebDriver(new URL("http://46.101.220.229:4444/wd/hub"), chromeOptions);
 
         System.out.println("***** Selenium Grid Chrome *****");
 
