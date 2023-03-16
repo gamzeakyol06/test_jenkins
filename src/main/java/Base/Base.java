3c14eb79787b424c192ed456820846e17d363f10
gamzeakyol06 <gamzeakyol06@gmail.com>
2023-03-16 15:00:40 +0300

    Merge remote-tracking branch 'github/main'
    # Conflicts:
    #	src/main/java/Base/Base.java

--- src/main/java/Base/Base.java
+++ src/main/java/Base/Base.java
@@ -3,24 +3,18 @@ package Base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class Base {
     protected static WebDriver driver;
 
     public final static String MAIN_PAGE_URL = "https://merchanttest.niso.dev/";
-    //private ChromeOptions ChromeOptions;
 
     public void beforemethod() throws MalformedURLException, InterruptedException {
 
         ChromeOptions chromeOptions = new ChromeOptions();
-
         chromeOptions.setCapability("browserName","chrome");
-        //chromeOptions.setCapability("platformName","LINUX");
+        chromeOptions.setCapability("platformName","LINUX");
         chromeOptions.addArguments("start-maximized");
         chromeOptions.addArguments("--headless");
         chromeOptions.addArguments("--no-sandbox");
@@ -29,48 +23,6 @@ public class Base {
         driver = new ChromeDriver(chromeOptions);
         System.setProperty("webdriver.chrome.driver","/src/main/driver/chromedriver_win32_x/chromedriver.exe");
         System.out.println(System.getProperty("webdriver.chrome.driver"));
-
-        /*ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setCapability("browserName","chrome");
-        chromeOptions.setCapability("browserVersion","110.0");
-        chromeOptions.setCapability("platformName","LINUX");
-        chromeOptions.setCapability("se:noVncPort",7900);
-        chromeOptions.setCapability("se:vncEnabled",true);
-        chromeOptions.addArguments("start-maximized");
-        //chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("--no-sandbox");
-        Thread.sleep(5000);*/
-
-        //driver = new RemoteWebDriver(new URL("http://46.101.220.229:4444/wd/hub"), chromeOptions);
-        //System.out.println("***** Selenium Grid Chrome *****");
-        /*ChromeOptions chromeOptions = new ChromeOptions();
-        //initialize chromeOptions
-        chromeOptions.setCapability("browserName", "chrome");
-        //Define on which browser you want to execute your tests.
-        chromeOptions.setCapability("platformName", "LINUX");
-        //Define in which mode your tests will run.
-        chromeOptions.addArguments("--headless");
-        //Define the platform on which you will execute your tests
-        WebDriver driver = new RemoteWebDriver(new URL("http://172.28.1.43:8080"), chromeOptions);*/
-        /*chromeOptions.addArguments("--whitelist-ip *");
-        chromeOptions.addArguments("--proxy-server='direct://'");
-        chromeOptions.addArguments("--proxy-bypass-list=*");
-        chromeOptions.addArguments("--ignore-certificate-errors");
-        chromeOptions.addArguments("test-type");
-        chromeOptions.addArguments("--start-maximized");
-        chromeOptions.addArguments("--disable-web-security");
-        chromeOptions.addArguments("--allow-running-insecure-content");
-        chromeOptions.setAcceptInsecureCerts(true);
-        chromeOptions.setHeadless(true);
-        chromeOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-        chromeOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-        //test.eldorturkey.com.tr
-        // test.eldorturkey.com.tr*/
-
-        //driver = new RemoteWebDriver(new URL("http://172.28.1.43:8080/wd/hub"),chromeOptions);
-
-        //driver.manage().window().maximize();
-
 
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
